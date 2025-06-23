5756825644c5e69dc0c5dea26b5c352026927ff4
Akshay Kadam <Akshay Kadam@ENIX-AKSHAY>
2025-06-23 16:38:54 +0530

    V1.0

---
+++ Voting.Client/Voting.client/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Voting.client.Data;
+using Voting.client.Models;
+using System.Diagnostics;
+
+namespace Voting.client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(ProductContext.Products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
